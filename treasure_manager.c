fec3093246d0ca24e0a0328b674e702b93c86845
Catalin Dogaru <catalindogaru@Catalins-MacBook-Air.local>
2025-04-09 09:18:49 +0300

    created the add function

--- treasure_manager.c
+++ treasure_manager.c
@@ -20,6 +20,46 @@ typedef struct {
     int value;
 } Treasure;
 
+void add(const char *hunt_id) {
+    mkdir(hunt_id, 0777);
+    char path[256];
+    snprintf(path, sizeof(path), "%s/treasures.dat", hunt_id);
+
+    FILE *f = fopen(path, "ab");
+    if (!f) { 
+        perror("fopen"); 
+        return; 
+    }
+
+    Treasure t;
+    printf("Username: ");
+    scanf("%s", t.username);
+    printf("Latitude: "); 
+    scanf("%f", &t.c.x);
+    printf("Longitude: "); 
+    scanf("%f", &t.c.y);
+    getchar();
+    printf("Clue: "); 
+    fgets(t.clue, sizeof(t.clue), stdin);
+    t.clue[strcspn(t.clue, "\n")] = 0;
+    printf("Value: "); 
+    scanf("%d", &t.value);
+
+    FILE *rf = fopen(path, "rb");
+    int count = 0; 
+    Treasure tmp;
+    if (rf) {
+        while (fread(&tmp, sizeof(Treasure), 1, rf) == 1) 
+        count++;
+        fclose(rf);
+    }
+
+    t.treasureID = count + 1;
+    fwrite(&t, sizeof(Treasure), 1, f);
+    fclose(f);
+    printf("Am adaugat comoara cu id-ul %d\n", t.treasureID);
+}
+
 int main(){
 
     return 0;
