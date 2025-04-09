0cf47513da5d20f296f4b1391937d955df90ba7f
Catalin Dogaru <catalindogaru@Catalins-MacBook-Air.local>
2025-04-09 09:09:19 +0300

    created Treasure and Coordinates struct

--- treasure_manager.c
+++ treasure_manager.c
@@ -5,6 +5,21 @@
 #include <sys/types.h>
 #include <sys/time.h>
 
+#define MAX_TREASURES 100
+
+typedef struct {
+    float x;
+    float y;
+} Coordinates;
+
+typedef struct {
+    int treasureID;
+    char username[100];
+    Coordinates c;
+    char clue[100];
+    int value;
+} Treasure;
+
 int main(){
 
     return 0;
